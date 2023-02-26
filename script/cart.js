1d0fc4dbc68db414d8f88caba5ad9c2c56c6b893
itsy <itsy@pandacrp.com>
2023-02-26 22:58:01 +0100

    Modification de l'identation

--- script/cart.js
+++ script/cart.js
@@ -1,30 +1,30 @@
-async function updateCart(response){
-    let qtt = 0;
-    await $('#badgeCart').html(response['quantity'])
-    if (response['quantity'] == 0){
-        $('#badgeCart').addClass('d-none')
-    } else {
-        $('#badgeCart').removeClass('d-none')
-    }
+async function updateCart(response) {
+  let qtt = 0;
+  await $("#badgeCart").html(response["quantity"]);
+  if (response["quantity"] == 0) {
+    $("#badgeCart").addClass("d-none");
+  } else {
+    $("#badgeCart").removeClass("d-none");
+  }
 }
 
 async function getCart() {
-    let cart = {};
-    await $.ajax({
-        url: "/api/Cart.php",
-        method: "POST",
-        data: {
-            'function': 'get_cart',
-        },
-        success: function(response){
-            updateCart(response);
-            cart = response;
-        }
-    })
-    return cart;
+  let cart = {};
+  await $.ajax({
+    url: "/api/Cart.php",
+    method: "POST",
+    data: {
+      function: "get_cart",
+    },
+    success: function (response) {
+      updateCart(response);
+      cart = response;
+    },
+  });
+  return cart;
 }
-async function makeOffCanvas(){
-    await $('body').append(`
+async function makeOffCanvas() {
+  await $("body").append(`
         <div class="offcanvas offcanvas-end" tabindex="-1" id="cartMenu">
           <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="offcanvasRightLabel"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart</h5>
@@ -34,25 +34,28 @@ async function makeOffCanvas(){
           </div>
         </div>
     `);
-    makeList()
+  makeList();
 }
 
-async function makeList(){
-    const cartList = await getCart();
-    $('#cartList').html();
-    Object.values(cartList.products).forEach(item => {
-        product = item.product;
-        quantity = item.quantity;
-        $('#cartList').append(`
+async function makeList() {
+  const cartList = await getCart();
+  $("#cartList").html();
+  Object.values(cartList.products).forEach((item) => {
+    product = item.product;
+    quantity = item.quantity;
+    $("#cartList").append(`
         <div class="d-flex">
             <img src="${product.imagePrincipal}" class="w-25">
             <div>
                 <h5>${product.nomProduit}</h5><div class="d-flex">
                 <div class="w-50 p-4">
                     <div class="input-group d-flex mt-3">
-                        <a style="flex: 1;" class="btn btn-primary" name="quantity" action="remove" role="button">-</a>
+                        <a style="flex: 1;" class="btn btn-primary" name="quantity" product-id="${product.id}" offcanvas="true" action="remove" role="button">-</a>
                         <input style="flex: 1;" type="number" name="quantity" class="form-control text-center" value="${quantity}" min="1">
-                        <a style="flex: 1;" class="btn btn-primary" name="quantity" action="add" role="button">+</a>
+                        <a style="flex: 1;" class="btn btn-primary" name="quantity" product-id="${product.id}" offcanvas="true" action="add" role="button">+</a>
+                    </div>
+                    <div>
+                        <a class="btn btn-danger" product-id="${product.id}" action="delete" name="quantity"><i class="fa fa-trash" aria-hidden="true"></i> Delete item</a>
                     </div>
                 </div>
                 <div class="d-flex flex-column">
@@ -61,83 +64,87 @@ async function makeList(){
                 </div>
             </div>
         </div>
-        `)
-    });
+        `);
+  });
 }
 
-async function clearCart(){
-    let qtt = 0;
-    await $.ajax({
-        url: "/api/Cart.php",
-        method: "POST",
-        data: {
-            'function': 'reset_cart',
-        },
-        success: function(response){
-            updateCart(response);
-            new Noty({
-                type: (!response.success?'error':'success'),
-                layout: 'topRight',
-                text: response.success??response.error,
-                timeout: 3e3
-            }).show();
-            qtt = response.quantity;
-        }
-    })
-    return qtt;
+async function clearCart() {
+  let qtt = 0;
+  await $.ajax({
+    url: "/api/Cart.php",
+    method: "POST",
+    data: {
+      function: "reset_cart",
+    },
+    success: function (response) {
+      updateCart(response);
+      new Noty({
+        type: !response.success ? "error" : "success",
+        layout: "topRight",
+        text: response.success ?? response.error,
+        timeout: 3e3,
+      }).show();
+      qtt = response.quantity;
+    },
+  });
+  return qtt;
 }
 
-async function addCart(productId, quantity = 1){
-    let qtt = 0;
-    await $.ajax({
-        url: "/api/Cart.php",
-        method: "POST",
-        data: {
-            'function': 'add_item',
-            'productId': productId,
-            'quantity': quantity
-        },
-        success: function(response){
-            updateCart(response);
-            new Noty({
-                type: (!response.success?'error':'success'),
-                layout: 'topRight',
-                text: response.success??response.error,
-                timeout: 3e3
-            }).show();
-            qtt = response.quantity;
-        }
-    })
-    return qtt;
+async function addCart(productId, quantity = 1) {
+  let qtt = 0;
+  await $.ajax({
+    url: "/api/Cart.php",
+    method: "POST",
+    data: {
+      function: "add_item",
+      productId: productId,
+      quantity: quantity,
+    },
+    success: function (response) {
+      updateCart(response);
+      new Noty({
+        type: !response.success ? "error" : "success",
+        layout: "topRight",
+        text: response.success ?? response.error,
+        timeout: 3e3,
+      }).show();
+      qtt = response.quantity;
+    },
+  });
+  return qtt;
 }
 
-async function removeCart(productId){
-    let qtt = 0;
-    await $.ajax({
-        url: "/api/Cart.php",
-        method: "POST",
-        data: {
-            'function': 'remove_item',
-            'productId': productId
-        },
-        success: function(response){
-            updateCart(response);
-            new Noty({
-                type: (!response.success?'error':'success'),
-                layout: 'topRight',
-                text: response.success??response.error,
-                timeout: 3e3
-            }).show();
-            qtt = response.quantity;
-        }
-    })
-    return qtt;
+async function removeCart(productId, quantity = 1) {
+  let qtt = 0;
+  await $.ajax({
+    url: "/api/Cart.php",
+    method: "POST",
+    data: {
+      function: "remove_item",
+      productId: productId,
+      quantity: quantity,
+    },
+    success: function (response) {
+      updateCart(response);
+      new Noty({
+        type: !response.success ? "error" : "success",
+        layout: "topRight",
+        text: response.success ?? response.error,
+        timeout: 3e3,
+      }).show();
+      qtt = response.quantity;
+    },
+  });
+  return qtt;
 }
-$(document).ready(function() {
-    makeOffCanvas();
-    getCart();
-    $(document).on('click', '#buy-it', async function(){
-        let qtt = await addCart($(this).attr('product-id'), $('input[name="quantity"]').val())
-        updateCart({quantity: qtt});
-    })
+$(document).ready(function () {
+  makeOffCanvas();
+  getCart();
+  $(document).on("click", "#buy-it", async function () {
+    let qtt = await addCart(
+      $(this).attr("product-id"),
+      $('input[name="quantity"]').val()
+    );
+    updateCart({ quantity: qtt });
+  });
 });
